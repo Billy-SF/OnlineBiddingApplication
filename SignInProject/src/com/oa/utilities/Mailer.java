ada67e3e3c15a364c174df4fcce04bf1cafdbe34
aroweeri <dylanlsutton@gmail.com>
2018-02-14 12:39:28 -0500

    Email sending class complete.

--- SignInProject/src/com/oa/utilities/Mailer.java
+++ SignInProject/src/com/oa/utilities/Mailer.java
@@ -1,54 +1,105 @@
 package com.oa.utilities;
 
-import java.util.Properties;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.util.Scanner;
+
+import com.oa.dao.Dao;
+import com.oa.helpers.User;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Mailer {
-	
-	private String recipient;
-	private String subject;
-	private String body;
-	
-	public Mailer(String recipient, String subject, String body) {
-		this.recipient = recipient;
-		this.subject = subject;
-		this.body = body;
-	}
-	
-	public boolean send() {
-		boolean noException;
-		
-		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "localhost");
-		props.put("mail.smtp.port", "25");
-		
-		Session session = Session.getDefaultInstance(props,
-				new javax.mail.Authenticator() {
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication("username","password");
-					}
-				});
-		
-		try {
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("from-email@gmail.com"));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
-			message.setSubject(subject);
-			message.setText(body);
 
-			Transport.send(message);
-			return true;
-		} catch (MessagingException e) {
+	private String recipient;
+	private String code;
+
+	public Mailer(String recipient, String code) {
+		this.recipient = recipient;
+		this.code = code;
+	}
+
+	public void send() {
+
+		boolean haveKey = true;
+		String apiKeyPath = null;
+		String apiKey = null;
+
+		/* get api key from file */
+		Connection conn = Dao.getConnection();
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+
+		try{
+			pst = conn.prepareStatement("select * from api_key");
+			rs = pst.executeQuery();
+			if(rs.next()) {
+				apiKeyPath = rs.getString("code");
+				File f = new File(apiKeyPath);
+				Scanner s = null;
+				try {
+					s = new Scanner(f);
+					if(s.hasNextLine()) {
+						apiKey = s.nextLine();
+					} else {
+						haveKey = false;
+					}
+				} catch (java.io.FileNotFoundException e) {
+					System.err.println("Unable to open file containing api key. Does it exist?");
+				}
+			} else {
+				haveKey = false;
+			}
+		}catch (Exception e) {
 			e.printStackTrace();
-			return false;
+		} finally {
+			if (pst != null) {
+				try {
+					pst.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		if (haveKey) {
+			try {
+				StringBuilder result = new StringBuilder();
+				result.append("https://api.elasticemail.com/v2/email/send?apikey=");
+				result.append(apiKey);
+				result.append("&from=verify@aro.science");
+				result.append("&subject=Verify+With+Ottawauction");
+				result.append("&bodyText=Your+verification+code+is:+");
+				result.append(code);
+				result.append("&to=");
+				result.append(recipient);
+
+				URL url = new URL(result.toString());
+				HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+				httpConn.setRequestMethod("GET");
+				BufferedReader rd = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
+				String line;
+				while ((line = rd.readLine()) != null) {
+					result.append(line);
+				}
+				rd.close();
+				System.out.println(result.toString());
+			} catch (java.net.MalformedURLException e) {
+			} catch (java.io.IOException e) {
+			}
 		}
 	}
 }
